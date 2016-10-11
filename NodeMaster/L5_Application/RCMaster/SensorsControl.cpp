a09e27e1eaeb41534277093a0deea72a0256720c
haroldo_psf <hubs.filho@gmail.com>
2016-10-10 20:34:45 -0700

    Initial infrastructure for Masternode with main classes: Coordinator and NodeControl base and derived classes

---
+++ NodeMaster/L5_Application/RCMaster/SensorsControl.cpp
@@ -0,0 +1,18 @@
+/*
+ * SensorsControl.cpp
+ *
+ *  Created on: Oct 10, 2016
+ *      Author: home
+ */
+
+#include <RCMaster/SensorsControl.h>
+
+SensorsControl::SensorsControl() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SensorsControl::~SensorsControl() {
+	// TODO Auto-generated destructor stub
+}
+
