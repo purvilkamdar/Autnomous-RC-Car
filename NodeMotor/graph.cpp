f8c7bf4263244def7715b24322c20aab81e40d99
Saketh Sai Narayana <n.sakethsai@gmail.com>
2016-10-15 16:26:16 -0700

    Colour definitions using the hex format similar to HTML

--- NodeMotor/graph.cpp
+++ NodeMotor/graph.cpp
@@ -6,5 +6,35 @@
  */
 
 
+/*
+ * This file is responsible to read and display the I/O data of the GPS Sensor, Compass module, Sonar Sensors
+ * and also display the speed of the RC car.
+ *
+ * */
+
+// defining color values
+#define YELLOW 0x00FFE0
+#define GREEN 0x00FF00
+#define BROWN 0x000033
+#define SKYBLUE 0xffb266
+#define BLACK 0x000000
+#define CYAN 0x0007FF
+#define RED 0xFF0000
+#define MAGENTA 0x00F81F
+#define WHITE 0xFFFFFF
+#define PURPLE 0xCC33FF
+
+#include "can.h"  /* necessary to read the CAN messages */
+#include "ssp1.h" /* Using SPI communication to display necessary elements on the LCD*/
+
+void compass_diagram(float longitude, float latitude, int direction){
+
+
+
+}
+
+
+
+
 
 
