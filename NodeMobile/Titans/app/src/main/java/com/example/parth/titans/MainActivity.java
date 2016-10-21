c38798b4ef240d520c096d50c59e44e6d0ef8b9e
PARTH PACHCHIGAR <PARTH PACHCHIGAR>
2016-10-20 22:04:31 -0700

    Added BLE connection code

--- NodeMobile/Titans/app/src/main/java/com/example/parth/titans/MainActivity.java
+++ NodeMobile/Titans/app/src/main/java/com/example/parth/titans/MainActivity.java
@@ -1,47 +1,163 @@
 package com.example.parth.titans;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+
+
 public class MainActivity extends AppCompatActivity {
-    private boolean btConnected=false;
+    private boolean btConnected = false,btOn=false;
     private BluetoothAdapter mBluetoothAdapter;
+    private String devices[]=new String[100];
+    private static int count=0;
+    private ImageButton bluetooth=null;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    BluetoothGattCallback gCallback=new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            super.onConnectionStateChange(gatt, status, newState);
+
+        }
+
+        @Override
+        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+            super.onCharacteristicChanged(gatt, characteristic);
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            super.onServicesDiscovered(gatt, status);
+        }
+    };
+    ScanCallback callback=new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+            devices[count]=result.getDevice().getAddress();
+            count++;
+            if(result.getDevice().getName()!=null) {
+                if (result.getDevice().getName().equalsIgnoreCase("HMSoft")) {
+                    result.getDevice().connectGatt(getApplicationContext(),true,gCallback);
+                    Log.i("PUP:", result.getDevice().getName());
+
+                    count = 0;
+                    Log.i("PUP:","END");
+                    bluetooth.setColorFilter(Color.GREEN);
+
+                    mBluetoothAdapter.getBluetoothLeScanner().stopScan(callback);
+
+                }
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
-            Toast.makeText(this, "BLE is not supported ... ", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "BLE is not supported ... ", Toast.LENGTH_LONG).show();
             finish();
         }
-        final ImageButton bluetooth=(ImageButton)findViewById(R.id.imageButton3);
+        bluetooth = (ImageButton) findViewById(R.id.imageButton3);
+        bluetooth.setColorFilter(Color.BLACK);
+
         bluetooth.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!btConnected) {
+                if (!btOn) {
                     final BluetoothManager bluetoothManager =
                             (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
                     mBluetoothAdapter = bluetoothManager.getAdapter();
                     if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
                         Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                        startActivityForResult(enableBtIntent, 1);
+                        startActivityForResult(enableBtIntent, 0);
                     }
-                    btConnected=true;
-                    bluetooth.setEnabled(false);
+                    else
+                    {
+                        bluetooth.setColorFilter(Color.RED);
+                    }
+                    btOn = true;
+                    Toast.makeText(getApplicationContext(), "Bluetooth is on", Toast.LENGTH_LONG);
+                    bluetooth.setColorFilter(Color.RED);
+                    //bluetooth.setEnabled(false);
+                } else {
+                    if (mBluetoothAdapter.getBluetoothLeScanner() != null) {
+                        mBluetoothAdapter.getBluetoothLeScanner().startScan(callback);
+
+                    } else {
+                        Toast.makeText(getApplicationContext(), "NULL SCANNER", Toast.LENGTH_SHORT);
+                    }
+
                 }
-                else
-                {
-                    Toast.makeText(getApplicationContext(), "Bluetooth is on", Toast.LENGTH_SHORT);
-                }
+
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
