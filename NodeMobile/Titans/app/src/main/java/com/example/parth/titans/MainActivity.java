f7daebd201891dbb0a707ba840df997081c445da
PARTH PACHCHIGAR <PARTH PACHCHIGAR>
2016-10-21 15:14:04 -0700

    Added BLE Service

--- NodeMobile/Titans/app/src/main/java/com/example/parth/titans/MainActivity.java
+++ NodeMobile/Titans/app/src/main/java/com/example/parth/titans/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.parth.titans;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
@@ -12,7 +14,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.graphics.ColorFilter;
 import android.net.Uri;
+import android.os.ParcelUuid;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -27,102 +32,117 @@ import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import android.os.Handler;
 
 
 public class MainActivity extends AppCompatActivity {
-    private boolean btConnected = false,btOn=false;
-    private BluetoothAdapter mBluetoothAdapter;
-    private String devices[]=new String[100];
-    private static int count=0;
-    private ImageButton bluetooth=null;
-    /**
+     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     private GoogleApiClient client;
-    BluetoothGattCallback gCallback=new BluetoothGattCallback() {
-        @Override
-        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
-            super.onConnectionStateChange(gatt, status, newState);
-
-        }
-
-        @Override
-        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
-            super.onCharacteristicChanged(gatt, characteristic);
-        }
-
-        @Override
-        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
-            super.onServicesDiscovered(gatt, status);
-        }
-    };
-    ScanCallback callback=new ScanCallback() {
-        @Override
-        public void onScanResult(int callbackType, ScanResult result) {
-            super.onScanResult(callbackType, result);
-            devices[count]=result.getDevice().getAddress();
-            count++;
-            if(result.getDevice().getName()!=null) {
-                if (result.getDevice().getName().equalsIgnoreCase("HMSoft")) {
-                    result.getDevice().connectGatt(getApplicationContext(),true,gCallback);
-                    Log.i("PUP:", result.getDevice().getName());
-
-                    count = 0;
-                    Log.i("PUP:","END");
-                    bluetooth.setColorFilter(Color.GREEN);
-
-                    mBluetoothAdapter.getBluetoothLeScanner().stopScan(callback);
-
-                }
-            }
-        }
-    };
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
-            Toast.makeText(this, "BLE is not supported ... ", Toast.LENGTH_LONG).show();
-            finish();
-        }
-        bluetooth = (ImageButton) findViewById(R.id.imageButton3);
-        bluetooth.setColorFilter(Color.BLACK);
-
-        bluetooth.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!btOn) {
-                    final BluetoothManager bluetoothManager =
-                            (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-                    mBluetoothAdapter = bluetoothManager.getAdapter();
-                    if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
-                        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                        startActivityForResult(enableBtIntent, 0);
-                    }
-                    else
-                    {
-                        bluetooth.setColorFilter(Color.RED);
-                    }
-                    btOn = true;
-                    Toast.makeText(getApplicationContext(), "Bluetooth is on", Toast.LENGTH_LONG);
-                    bluetooth.setColorFilter(Color.RED);
-                    //bluetooth.setEnabled(false);
-                } else {
-                    if (mBluetoothAdapter.getBluetoothLeScanner() != null) {
-                        mBluetoothAdapter.getBluetoothLeScanner().startScan(callback);
-
-                    } else {
-                        Toast.makeText(getApplicationContext(), "NULL SCANNER", Toast.LENGTH_SHORT);
-                    }
-
-                }
-
-            }
-        });
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
+
+
+    // Variable declarations
+    private BluetoothAdapter btAdapter;
+    private BluetoothManager btManager;
+    private ImageButton btButton;
+    private Handler handler;
+    private final int SCAN_PERIOD=1000;
+    private  boolean scanning;
+    private BluetoothDevice bleChip;
+    // BLE Scanning callback which is called when device is scanning
+    private BluetoothAdapter.LeScanCallback mLeScanCallback =
+            new BluetoothAdapter.LeScanCallback() {
+
+                @Override
+                public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            bleChip=device;
+                            if(bleChip.getAddress().equalsIgnoreCase("00:0E:0B:0B:3E:28"))
+                            if (scanning) {
+                                btAdapter.stopLeScan(mLeScanCallback);
+                                Log.i("TITANS:",bleChip.getName());
+                                scanning = false;
+                            }
+                        }
+                    });
+                }
+            };
+
+
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    scanning = false;
+                    btAdapter.stopLeScan(mLeScanCallback);
+                    invalidateOptionsMenu();
+                }
+            }, SCAN_PERIOD);
+
+            scanning = true;
+            btAdapter.startLeScan(mLeScanCallback);
+        } else {
+            scanning = false;
+            btAdapter.stopLeScan(mLeScanCallback);
+        }
+        invalidateOptionsMenu();
+    }
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btButton=(ImageButton)findViewById(R.id.imageButton);
+
+        handler=new Handler();
+
+        // To check whether device supports BLE or not
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, "Device doesn't support BLE", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        // Initialize Bluetooth Adapter
+        btManager =(BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        btAdapter = btManager.getAdapter();
+
+        // To check whether device supports Bluetooth or not
+        if (btAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not Supported", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!btAdapter.isEnabled()) {
+            if (!btAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 0);
+            }
+        }
+        scanLeDevice(true);
     }
 
     /**
@@ -160,4 +180,5 @@ public class MainActivity extends AppCompatActivity {
         AppIndex.AppIndexApi.end(client, getIndexApiAction());
         client.disconnect();
     }
+
 }
