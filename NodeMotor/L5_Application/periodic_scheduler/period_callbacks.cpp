d885fcbe63904acb20aae7e9e1738e425a5d01a3
santpsoy <santpsoy@gmail.com>
2016-10-25 21:52:05 -0700

    motor controller code for speed and steering

--- NodeMotor/L5_Application/periodic_scheduler/period_callbacks.cpp
+++ NodeMotor/L5_Application/periodic_scheduler/period_callbacks.cpp
@@ -28,9 +28,50 @@
  * do must be completed within 1ms.  Running over the time slot will reset the system.
  */
 
-#include <stdint.h>
+#include "stdint.h"
 #include "io.hpp"
 #include "periodic_callback.h"
+#include "../_can_dbc/generated_can.h"
+#include "can.h"
+#include "stdio.h"
+#include "utilities.h"
+#include "string.h"
+#include "lpc_pwm.hpp"
+
+#define SPEED_LEVEL_STOP 				14
+#define SPEED_LEVEL_SLOW 				15.8
+#define SPEED_LEVEL_MEDIUM 				18
+#define SPEED_LEVEL_FAST 				20
+
+#define STEERING_POS_LEFT				10
+#define STEERING_POS_SLIGHT_LEFT		12
+#define STEERING_POS_CENTER				14
+#define STEERING_POS_SLIGHT_RIGHT		16
+#define STEERING_POS_RIGHT				18
+
+
+PWM carSteer(PWM::pwm1, 8);
+PWM carSpeed(PWM::pwm2, 8);
+
+const MASTER_HB_t master_mia_msg = {0};
+const uint32_t master_mia_ms = 10;
+
+const uint32_t            MASTER_HB__MIA_MS = 300;
+const MASTER_HB_t         MASTER_HB__MIA_MSG = { 8 };
+
+MASTER_HB_t master_can_msg = { 0 };
+
+bool dbc_app_send_can_msg(uint32_t mid, uint8_t dlc, uint8_t bytes[8])
+{
+    can_msg_t can_msg = { 0 };
+    can_msg.msg_id                = mid;
+   // printf("\n MID %lu",mid);
+    can_msg.frame_fields.data_len = dlc;
+    //printf("\n DLC %d",dlc);
+    memcpy(can_msg.data.bytes, bytes, dlc);
+
+    return CAN_tx(can1, &can_msg, 0);
+}
 
 //added this line in dev branch
 
@@ -48,6 +89,12 @@ const uint32_t PERIOD_DISPATCHER_TASK_STACK_SIZE_BYTES = (512 * 3);
 /// Called once before the RTOS is started, this is a good place to initialize things once
 bool period_init(void)
 {
+    CAN_init(can1,100,3,3,NULL,NULL);
+    //CAN_bypass_filter_accept_all_msgs();
+    const can_std_id_t slist[]  = { CAN_gen_sid(can1, 0x020),   // Acknowledgment from the nodes that received sensor reading
+									  CAN_gen_sid(can1, 0x021) }; // Only 1 ID is expected, hence small range
+    CAN_setup_filter(slist, 2, NULL, 0, NULL, 0, NULL, 0);
+    CAN_reset_bus(can1);
     return true; // Must return true upon success
 }
 
@@ -64,20 +111,111 @@ bool period_reg_tlm(void)
  * The argument 'count' is the number of times each periodic task is called.
  */
 
+
 void period_1Hz(uint32_t count)
 {
     LE.toggle(1);
 }
 
+MASTER_HB_t master_hb_msg = { 0 };
+MOTOR_STATUS_t motor_msg={0};
+
+
 void period_10Hz(uint32_t count)
 {
-	LE.toggle(1);
-    LE.toggle(2);
+	can_msg_t can_msg;
+
+	        // Empty all of the queued, and received messages within the last 10ms (100Hz callback frequency)
+	       while (CAN_rx(can1, &can_msg, 0))
+	        {
+	            // Form the message header from the metadata of the arriving message
+	            dbc_msg_hdr_t can_msg_hdr;
+	            can_msg_hdr.dlc = can_msg.frame_fields.data_len;
+	            can_msg_hdr.mid = can_msg.msg_id;
+
+	            // Attempt to decode the message (brute force, but should use switch/case with MID)
+
+	            motor_msg.MOTOR_STATUS_speed_mph = 9.0;
+	            if(can_msg_hdr.mid==0x20)
+	            {
+	            	printf("Received command from master\n");
+	            	 dbc_decode_MASTER_HB(&master_hb_msg, can_msg.data.bytes, &can_msg_hdr);
+	            	 printf("Data received from maaster for speed is : %d\n",master_hb_msg.MASTER_SPEED_cmd);
+	            	 printf("Data received from maaste for steer is : %d\n",master_hb_msg.MASTER_STEER_cmd);
+	            	 printf("Data length received from maaste is : %d\n",can_msg_hdr.dlc);
+	            if(master_hb_msg.MASTER_SPEED_cmd == 1)
+	            {
+	            	carSpeed.set(SPEED_LEVEL_MEDIUM);
+	            	//printf("Got master command\n");
+
+	            		if(master_hb_msg.MASTER_STEER_cmd == 0)
+	            		{
+	            			carSteer.set(STEERING_POS_LEFT);
+	            		}
+
+	            		if(master_hb_msg.MASTER_STEER_cmd == 1)
+	            		{
+	            			carSteer.set(STEERING_POS_SLIGHT_LEFT);
+	            		}
+
+	            		if(master_hb_msg.MASTER_STEER_cmd == 2)
+	            		{
+	            			carSteer.set(STEERING_POS_CENTER);
+	            		}
+
+	            		if(master_hb_msg.MASTER_STEER_cmd == 3)
+	            		{
+	            			carSteer.set(STEERING_POS_SLIGHT_RIGHT);
+	            		}
+
+	            		if(master_hb_msg.MASTER_STEER_cmd == 4)
+	            		{
+	            			carSteer.set(STEERING_POS_RIGHT);
+	            		}
+/*
+
+	            		if(master_hb_msg.MASTER_STEER_cmd == 5)
+	            		{
+	            			carSteer.set(STEERING_POS_SLIGHT_RIGHT);
+	            		}
+
+	            		if((count % 8) == 6)
+	            		{
+	            			carSteer.set(STEERING_POS_CENTER);
+	            		}
+
+	            		if((count % 8) == 7)
+	            		{
+	            			carSteer.set(STEERING_POS_SLIGHT_LEFT);
+	            		}
+*/
+	            	LD.setNumber(master_hb_msg.MASTER_SPEED_cmd);
+
+
+	            }
+	            else
+	            {
+	            	carSpeed.set(SPEED_LEVEL_STOP);
+	            	printf("Master Command is 0\n");
+	            	LD.setNumber(master_hb_msg.MASTER_SPEED_cmd);
+	            }
+	            dbc_encode_and_send_MOTOR_STATUS(&motor_msg);
+	            //printf("Sent to CSN");
+	           }
+
+	        }
+	        if(CAN_is_bus_off(can1))
+	        {
+	        	LD.setNumber(4);
+	            CAN_reset_bus(can1);
+	        }
+
+	//LE.toggle(2);
 }
 
 void period_100Hz(uint32_t count)
 {
-    LE.toggle(3);
+   // LE.toggle(3);
 }
 
 // 1Khz (1ms) is only run if Periodic Dispatcher was configured to run it at main():
