216dd33b3847fcfdebe0b2c74d7717fcd6880b4a
kayal2810 <kayal2810@gmail.com>
2016-11-06 13:08:19 -0800

    Modified LCD_Display.cpp to add LCD Rx & Tx scheduler tasks

--- NodeMotor/L5_Application/source/LCD_Display/LCD_Display.cpp
+++ NodeMotor/L5_Application/source/LCD_Display/LCD_Display.cpp
@@ -4,29 +4,10 @@
  *  Created on: Nov 6, 2016
  *      Author: Kay
  */
-#include <stdio.h>
+
 #include "LCD_Display.hpp"
-#include "uart3.hpp"
-
-LCD_touch_Task::LCD_touch_Task() :
-    scheduler_task("LCD_touch_Task", 3 * 512, PRIORITY_HIGH)
-{
-    /**
-     * This is optional such that run() will be called every 3000ms.
-     * If you don't do this, run() will be called without a delay.
-     */
-    setRunDuration(500);
-}
-
-bool LCD_touch_Task::run(void *p)
-{
-
-    return true;
-}
 
 
-
-// RX Task
 LCD_Rx_Task::LCD_Rx_Task() :
     scheduler_task("LCD_Rx_Task", 3 * 512, PRIORITY_HIGH)
 {
@@ -41,4 +22,46 @@ bool LCD_Rx_Task::run(void *p)
 {
 
     return true;
+}
+
+
+
+// RX Task
+LCD_Tx_Task::LCD_Tx_Task() :
+    scheduler_task("LCD_Tx_Task", 3 * 512, PRIORITY_HIGH)
+{
+    /**
+     * This is optional such that run() will be called every 3000ms.
+     * If you don't do this, run() will be called without a delay.
+     */
+	ack = 0x00;
+    setRunDuration(500);
+
+}
+
+
+bool LCD_Tx_Task::run(void *p)
+{
+
+
+    return true;
+}
+
+
+void LCD_Tx_Task::write_LCD(char command,char obj_type,char obj_idx,char arr[],char len)
+{
+	int checksum = command ^ obj_type ^ obj_idx;
+	LCD_Rx.putChar(command,0);
+	LCD_Rx.putChar(obj_type,0);
+	LCD_Rx.putChar(obj_idx,0);
+
+	for(int i=0;i<len;i++)
+	{
+		LCD_Rx.putChar(arr[i],0);
+		checksum ^= arr[i];
+	}
+
+	LCD_Rx.putChar(checksum ,0); //checksum
+	LCD_Rx.getChar(&ack,10); // Rx ACK/NACK from LCD
+
 }
