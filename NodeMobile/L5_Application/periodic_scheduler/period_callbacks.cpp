52df26910ab850524d90bfa2cedc2d9213cf369a
Purvil Kamdar <purvilkamdar92@gmail.com>
2016-11-23 01:34:32 -0800

    Sending co-ordinates to master node

--- NodeMobile/L5_Application/periodic_scheduler/period_callbacks.cpp
+++ NodeMobile/L5_Application/periodic_scheduler/period_callbacks.cpp
@@ -37,6 +37,10 @@
 #include "can.h"
 #include "_can_dbc/generated_can.h"
 #include <iostream>
+#include<vector>
+#include <file_logger.h>
+#include <semphr.h>
+using namespace std;
 /// This is the stack size used for each of the period tasks (1Hz, 10Hz, 100Hz, and 1000Hz)
 const uint32_t PERIOD_TASKS_STACK_SIZE_BYTES = (512 * 4);
 
@@ -47,24 +51,28 @@ const uint32_t PERIOD_TASKS_STACK_SIZE_BYTES = (512 * 4);
  * printf inside these functions, you need about 1500 bytes minimum
  */
 const uint32_t PERIOD_DISPATCHER_TASK_STACK_SIZE_BYTES = (512 * 3);
-
-/**
- * TODO : Jon : Clean up declarations below either by making the variable names
- *              more descriptive or add comments to them
- */
 Uart2 &u2 = Uart2::getInstance();
-//char* msg = new char[10];
-char *size = new char[2];
+char *size	= new char[2];
+bool size_flag=0;
 bool start = 0;
 bool stop = 0;
-bool last_co = 0;
+bool last_flag = 0;
+bool last_co=0;
+bool match_flag=0;
 int MIA_Unit=0;
-int left=99,right=99,center=99,rear=99;
-float latitude=0,longitude=0,speed=0;
-int n;
-float* s_latitude = new float[40];
-float* s_longitude = new float [40];
-int lat_counter=0, longi_counter=0,counter=0,last_counter=0;
+int my_left=99,my_right=99,center=99,rear=99;
+float latitude=0,longitude=0,speed=0,temporary_latitude_value=0,temporary_longitude_value=0;
+volatile int n=0;
+int lat1,lat2,lat3,lat4,lat5,lat6,lon1,lon2,lon3,lon4,lon5,lon6;
+char a[1];
+//int s_latitude[100];
+//int s_longitude[100];
+float* s_latitude = new float[100];
+float* s_longitude = new float[100];
+int count_semaphore=0;
+int counter=0;
+volatile int print_counter=0;
+volatile int lat_counter=0;
 extern const uint32_t MASTER_HB__MIA_MS = 3000;
 extern const MASTER_HB_t MASTER_HB__MIA_MSG = { 0 };
 extern const uint32_t SENSOR_DATA__MIA_MS = 3000;
@@ -73,7 +81,8 @@ extern const uint32_t GPS_Data__MIA_MS = 3000;
 extern const GPS_Data_t GPS_Data__MIA_MSG = { 0.000000, 0.000000 };
 extern const uint32_t MOTOR_STATUS__MIA_MS=3000;
 extern const MOTOR_STATUS_t MOTOR_STATUS__MIA_MSG={0};
-
+int i=0;
+static SemaphoreHandle_t start_sending=0;
 
 MASTER_HB_t heartbeat = { 0 };
 SENSOR_DATA_t sensordata={0};
@@ -103,12 +112,18 @@ void overrun(uint32_t arg) {
 
 /// Called once before the RTOS is started, this is a good place to initialize things once
 bool period_init(void) {
+	for(i=0;i<100;i++)
+	{
+		s_latitude[i]=0;
+		s_longitude[i]=0;
+	}
 	can_void_func_t busOff = busoff;
 	can_void_func_t overr = overrun;
 	u2.init(115200);
 	CAN_init(can1, 100, 10, 10, busOff, overr);
 	CAN_bypass_filter_accept_all_msgs();
 	CAN_reset_bus(can1);
+	vSemaphoreCreateBinary(start_sending);
 	return true; // Must return true upon success
 }
 
@@ -131,19 +146,23 @@ void period_1Hz(uint32_t count) {
 
 void period_10Hz(uint32_t count) {
 
-	//if(start)
-	//{
-		//LD.setNumber(10);
-		//if(u2.gets(msg,8,0))
-		//printf("%s", msg);
-	//}
 
-    /**
-     * TODO: Jon : Put all of this code into a single static function
-     */
+	if(xSemaphoreTake(start_sending,0))
+	{
+		//This is the code for printing the app co-ordinates. Uncomment it in case of verification.
+	/*if(n!=0 && last_flag)
+	{
+		if(print_counter<lat_counter)
+		{
+			printf("The value of n is:%d \n",n);
+			printf("The value of counter is %d \n",lat_counter);
+			printf("%d) Co-ordinates=%f,%f \n",print_counter,s_latitude[print_counter],s_longitude[print_counter]);
+		}
+		print_counter++;
+	}*/
 
 	char* data=new char[20];
-	sprintf(data,"L%02dR%02dC%02dB%02dSPD%0.3f",left,right,center,rear,speed);
+	sprintf(data,"L%02dR%02dC%02dB%02dSPD%0.3f",my_left,my_right,center,rear,speed);
 	u2.put(data, 0);
 	sprintf(data,"LAT=%16.6f",latitude);
 	//u2.put(data,0);
@@ -168,39 +187,45 @@ void period_10Hz(uint32_t count) {
 		if(can_msg_hdr.mid == 0x20)
 		{
 			dbc_decode_MASTER_HB(&heartbeat, can_msg.data.bytes, &can_msg_hdr);
-			if(last_counter!=0 && last_counter<=n)
-					{ // TODO : Jon : nit: spacing
-						start_cmd.APP_ROUTE_latitude=s_latitude[counter];
-						start_cmd.APP_ROUTE_longitude=s_longitude[counter];
-						if(counter!=lat_counter-1)
-						{
-							start_cmd.APP_FINAL_COORDINATE=0;
-						}
-						if(last_counter==n)
-						{
-							start_cmd.APP_FINAL_COORDINATE=1;
-							lat_counter=0;
-							longi_counter=0;
-							counter=0;
-							last_co=0;
-							LD.setNumber(10);
-							LE.off(2);
-						}
-						last_counter++;
-						counter++;
-						if(counter>=40)
-							counter=0;
-					}
+			//n is the number of the co-ordinates from the APP & last_flag is true if last is received from the app
+			if(n!=0 && last_flag)
+			{
+				start_cmd.APP_COORDINATE_READY=1;
+			}
+			else
+			{
+				start_cmd.APP_COORDINATE_READY=0;
+			}
+			if(heartbeat.MASTER_START_COORD==1)
+			{
+				//counter is the iterator for the array and (lat_counter-1) is the number of co-ordinates after
+				//removing the repeated co-ordinates. lat_counter value & n will not be the same always.
+				if(counter<lat_counter)
+				{
+					start_cmd.APP_ROUTE_latitude=s_latitude[counter];
+					start_cmd.APP_ROUTE_longitude=s_longitude[counter];
+					if(print_counter==(lat_counter-1))
+						start_cmd.APP_FINAL_COORDINATE=1;
+					else
+						start_cmd.APP_FINAL_COORDINATE=0;
+					counter++;
+				}
+				else
+				{
+					start_cmd.APP_FINAL_COORDINATE=0;
+					start_cmd.APP_COORDINATE_READY=0;
+				}
+			}
 			dbc_encode_and_send_APP_START_STOP(&start_cmd);
 		}
 		if(can_msg_hdr.mid == 0x10){
 			dbc_decode_SENSOR_DATA(&sensordata, can_msg.data.bytes,&can_msg_hdr);
-			left=sensordata.SENSOR_left_sensor;
+			my_left=sensordata.SENSOR_left_sensor;
 			center=sensordata.SENSOR_middle_sensor;
-			right=sensordata.SENSOR_right_sensor;
-			if(left>99){left=99;}
+			my_right=sensordata.SENSOR_right_sensor;
+			if(my_left>99){my_left=99;}
 			if(center>99){center=99;}
-			if(right>99){right=99;}
+			if(my_right>99){my_right=99;}
 		}
 		if(can_msg_hdr.mid==0x30){
 			dbc_decode_MOTOR_STATUS(&motorstatus,can_msg.data.bytes,&can_msg_hdr);
@@ -216,7 +241,6 @@ void period_10Hz(uint32_t count) {
 			longitude=gpsdata.GPS_READOUT_longitude;
 		}
 	}
-	// TODO: Jon : handle_mia_status_leds();
 	if (dbc_handle_mia_MASTER_HB(&heartbeat, 100)) {
 		LD.clear();
 		LD.setRightDigit('1');
@@ -239,122 +263,103 @@ void period_10Hz(uint32_t count) {
 	}
 
 }
+}
 
 void period_100Hz(uint32_t count) {
-    /**
-     * TODO: Jon : Put all of this code into a single static function
-     */
 
-	//APP_START_STOP_t data = {0};
-	//data.APP_FINAL_COORDINATE=0;
-	char *stat=new char[8];
-		u2.gets(stat,8,0);
-		printf("String: %s",stat);
-		if(strcmp(stat,"start")==0) {
+
+	char *stat=new char[14];
+			u2.gets(stat,14,0);
+
+				if(strcmp(stat,"start")==0) {
 					start=1;
-					//send_lat_long=1;
+					lat_counter=0;
+					print_counter=0;
 					stop=0;
+					size_flag=0;
+					n=0;
 					LE.on(4);
-				}
-				if(strcmp(stat,"stop")==0) {
+					}
+
+				else if(strcmp(stat,"stop")==0) {
 					stop=1;
+					lat_counter=0;
+					counter=0;
 					start=0;
+					size_flag=0;
+					last_flag=0;
 					LE.off(4);
+					LE.off(2);
+					LE.off(3);
+					LE.off(1);
+					}
+
+				if(size_flag)
+				{
+					LE.on(3);
+					size_flag=0;
+					sscanf(stat,"%d",&n);
 				}
-		if(stat[0]=='S' && stat[1]=='i')
-		{
-			printf("%s",stat);
-			if(strlen(stat)>6)
-			{
-				char b[1];
-				b[0]=stat[5];
-				char c[1];
-				c[0]=stat[6];
-				strncat(size,b,1);
-				strncat(size,c,1);
-				sscanf(stat, "%d", &n);
-				printf("%d",n);
-				//delete[] s_longitude;
-				//delete[] s_latitude;
-				//s_longitude=new float[n];
-				//s_latitude=new float[n];
-			}
-			else if(strlen(stat)==6)
-			{
-				char d[1];
-				d[0]=stat[5];
-				strncat(size,d,1);
-				sscanf(stat, "%d", &n);
-				printf("%d",n);
-				//delete[] s_longitude;
-				//delete[] s_latitude;
-				//s_longitude=new float[n];
-				//s_latitude=new float[n];
-			}
-			//LE.on(3);
 
-		}
-		if(strcmp(stat,"Last")==0)
-		{
-			last_co=1;
-			LE.on(2);
-			//data.APP_FINAL_COORDINATE=1;
+				if(strcmp(stat,"Size")==0)
+					{
+						size_flag=1;
+					}
+				 if(strcmp(stat,"Last")==0)
+				{
+					LE.on(2);
+					last_flag=1;
+				}
+
+				if(stat[0]=='#' || stat[1]=='#')
+				{
+					match_flag=0;
+					lat1=stat[1]-'0';
+					lat2=stat[2]-'0';
+					lat3=stat[3]-'0';
+					lat4=stat[4]-'0';
+					lat5=stat[5]-'0';
+					lat6=stat[6]-'0';
+
+					lon1=stat[8]-'0';
+					lon2=stat[9]-'0';
+					lon3=stat[10]-'0';
+					lon4=stat[11]-'0';
+					lon5=stat[12]-'0';
+					lon6=stat[13]-'0';
 
 
-		}
-		else if(stat[0]=='-')
-		{
-			//LE.on(1);
-			s_longitude[longi_counter]=atof(stat);
-			printf("Longitude: %f",s_longitude[longi_counter]);
-			longi_counter++;
-			if(longi_counter>=40)
-				longi_counter=0;
-			//data.APP_ROUTE_longitude=s_longitude;
-		}
-		else if(stat[0]=='3')
-		{
-			//LE.off(1);
-			s_latitude[lat_counter]=atof(stat);
-			printf("Latitude: %f",s_latitude[lat_counter]);
-			lat_counter++;
-			if(lat_counter>=40)
-				lat_counter=0;
-			//data.APP_ROUTE_latitude=s_latitude;
-		}
-		/*if(strcmp(stat,"start")==0)
-			LE.on(1);
-		else if(strcmp(stat,"stop")==0)
-			LE.on(2);*/
+					temporary_latitude_value=37+(lat1*0.1)+(lat2*0.01)+(lat3*0.001)+(lat4*0.0001)+(lat5*0.00001)+(lat6*0.000001);
+					temporary_longitude_value=121+(lon1*0.1)+(lon2*0.01)+(lon3*0.001)+(lon4*0.0001)+(lon5*0.00001)+(lon6*0.000001);
+					int repeat_counter;
+					for(repeat_counter=0;repeat_counter<lat_counter;repeat_counter++)
+					{
+						if((s_latitude[repeat_counter]==temporary_latitude_value) && (s_longitude[repeat_counter]==temporary_longitude_value))
+						{
+							match_flag=1;
+						}
 
+					}
+					if(match_flag==0)
+					{
+						LE.on(1);
+						s_latitude[lat_counter]=temporary_latitude_value;
+						s_longitude[lat_counter]=temporary_longitude_value;
+						lat_counter++;
+					}
+				}
 
-	/*LE.off(2);
-	char* msg = new char[8];
+					delete[] stat;
+					if(last_flag)
+					{
+						xSemaphoreGive(start_sending);
+					}
 
-	if(u2.getChar(msg,0))
-		LE.on(2);
-	printf("\nReceived:%c",msg[0]);
-	//puts(msg);
-
-	if(msg[0]=='s')
-		LE.on(1);
-
-	if(u2.gets(msg,5,0)) {
-		LE.on(1);
-*/
-
-
-
-
-
-	delete[] stat;
-
-	//LE.toggle(3);
 }
 
 // 1Khz (1ms) is only run if Periodic Dispatcher was configured to run it at main():
 // scheduler_add_task(new periodicSchedulerTask(run_1Khz = true));
 void period_1000Hz(uint32_t count) {
-	//LE.toggle(4);
+
 }
 
